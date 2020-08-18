1d6851c8193b40e8dd71ba6041d48dab510310b4
telesensdev <bdmtelesens@gmail.com>
2020-08-18 17:57:23 +0300

    feature: get query in Search page

--- pages/search.js
+++ pages/search.js
@@ -1,3 +1,16 @@
-export default () => (
-  <div>Search</div>
-);
+import { wrapper } from "../store";
+import { getCategories } from "../store/categories/action";
+
+const Search = () => {
+
+    return (
+        <div>Search</div>
+    )
+}
+
+export const getServerSideProps = wrapper.getServerSideProps(async ({ store, query }) => {
+        console.log(query);
+    }
+)
+
+export default Search;
